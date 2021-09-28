821af520a0770ab3888d789e4f921224edeb625b
CeAlCuadrado <cealcuadrado@outlook.com>
2021-09-28 15:19:27 -0300

    Definido state del componente Layout

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,10 +1,22 @@
 import React from 'react';
 
 class Layout extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            header: 'Título de la aplicación',
+            subHeader: 'Subtítulo de la aplicación',
+            content: 'Contenido'
+        };
+    }
+
     render() {
         return (
             <div className="container py-2">
-                <p>Layout</p>
+                <h1>{this.state.header}</h1>
+                <h2>{this.state.subHeader}</h2>
+                <p>{this.state.content}</p>
             </div>
         )
     }
