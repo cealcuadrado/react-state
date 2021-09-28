c726a9ed721044a8055fa8145cc373794551c781
CeAlCuadrado <cealcuadrado@outlook.com>
2021-09-28 15:13:04 -0300

    Instaladas dependencias, creado layout

---
+++ src/layout/Layout.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Layout extends React.Component {
+    render() {
+        return (
+            <div className="container py-2">
+                <p>Layout</p>
+            </div>
+        )
+    }
+}
+
+export default Layout;
